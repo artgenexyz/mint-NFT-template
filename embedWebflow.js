7c02912380e5c2e2c5834fe6bafbdf0695febf32
theshadowagent <shadowagent1st@gmail.com>
2021-07-17 00:06:05 +0300

    Fix embedWebflow

--- embedWebflow.js
+++ embedWebflow.js
@@ -1,3 +1,3 @@
 <script src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/buildshipper/generativeNFT-template@main/connectWallet.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/buildshipper/generativeNFT-template@main/contract.js"></script>
+<script type="module" src="https://cdn.jsdelivr.net/gh/buildshipper/generativeNFT-template@main/connectWallet.js"></script>
+<script type="module" src="https://cdn.jsdelivr.net/gh/buildshipper/generativeNFT-template@main/contract.js"></script>
