94465e8eb2e2262bae4dc4ce957780f9864b3ba4
theshadowagent <shadowagent1st@gmail.com>
2021-08-10 22:03:56 +0100

    Fix CDN urls

--- embedWebflow.js
+++ embedWebflow.js
@@ -1,4 +1,4 @@
 <script src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js"></script>
-<script type="module" src="https://cdn.jsdelivr.net/gh/buildshipper/generativeNFT-template@main/connectWallet.js"></script>
-<script type="module" src="https://cdn.jsdelivr.net/gh/buildshipper/generativeNFT-template@main/contract.js"></script>
-<script type="module" src="https://cdn.jsdelivr.net/gh/buildshipper/generativeNFT-template@main/mint.js"></script>
+<script type="module" src="https://cdn.jsdelivr.net/gh/buildship-dev/generativeNFT-template@main/connectWallet.js"></script>
+<script type="module" src="https://cdn.jsdelivr.net/gh/buildship-dev/generativeNFT-template@main/contract.js"></script>
+<script type="module" src="https://cdn.jsdelivr.net/gh/buildship-dev/generativeNFT-template@main/mint.js"></script>
